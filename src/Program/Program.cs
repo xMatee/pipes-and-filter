511c4404e15e89fd7bed1d6d592c6d5a4aa2375a
xMatee <110862698+xMatee@users.noreply.github.com>
2022-10-31 21:50:39 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
+
+namespace CompAndDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
